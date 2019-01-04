59422cbe2b6d89e5f08833f31f3a85eefc7f7144
panqian <455234037@qq.com>
2019-01-04 18:51:48 +0800

    init

--- src/main/java/cn/xxywithpq/SimplifyCacheApplication.java
+++ src/main/java/cn/xxywithpq/SimplifyCacheApplication.java
@@ -1,9 +1,7 @@
 package cn.xxywithpq;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
@@ -14,18 +12,5 @@ public class SimplifyCacheApplication {
         SpringApplication.run(SimplifyCacheApplication.class, args);
     }
 
-//    @Bean
-//    RedissonClient redissonClient() {
-//        Config config = new Config();
-//        SingleServerConfig serverConfig = config.useSingleServer()
-//                .setAddress("redis://" + redisProperties.getHost() + ":" + redisProperties.getPort())
-//                .setTimeout((int) (redisProperties.getTimeout().getSeconds() * 1000))
-//                .setDatabase(redisProperties.getDatabase());
-//
-//        if (StringUtils.isNotBlank(redisProperties.getPassword())) {
-//            serverConfig.setPassword(redisProperties.getPassword());
-//        }
-//        return Redisson.create(config);
-//    }
 }
 
